3d23b73a1c6f81394b58b311c545d101c2dcc1a8
Aryse Tansy <medictansy@gmail.com>
2022-12-03 18:43:05 -0500

    Catch Block Err Feedback

--- client/src/components/Dashboard/Dashboard.tsx
+++ client/src/components/Dashboard/Dashboard.tsx
@@ -3,14 +3,21 @@ import { useContext } from "react";
 import { UserCtx } from "../../features/user-ctx";
 import Nav from "../Nav/Nav";
 import LoanList from "../LoanList/LoanList";
+import { LoanActionCtx } from "../../features/loan-action-ctx";
 
 const Dashboard: React.FC = () => {
   const userMgr = useContext(UserCtx);
+  const loanActMgr = useContext(LoanActionCtx);
 
   return (
     <section className={classes.section}>
       <h2 className={classes.h2}>{userMgr.currentUser.user}'s</h2>
       <p className={classes.p}>Money Moves</p>
+      {loanActMgr.serverErr && (
+        <p className={classes.feedback}>
+          Sorry, something went wrong, please try again.
+        </p>
+      )}
       <LoanList />
       <Nav />
     </section>
